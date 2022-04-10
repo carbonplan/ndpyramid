c5fc3fee943a6eed16bc4718a27528c6edec39ea
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-04-10 00:08:09 -0600

    update tests

--- tests/test_pyramids.py
+++ tests/test_pyramids.py
@@ -4,7 +4,7 @@ import xarray as xr
 from zarr.storage import MemoryStore
 
 from ndpyramid import pyramid_coarsen, pyramid_regrid, pyramid_reproject
-from ndpyramid.regrid import make_grid_ds
+from ndpyramid.regrid import generate_weights_pyramid, make_grid_ds
 
 
 @pytest.fixture
@@ -49,8 +49,30 @@ def test_regridded_pyramid(temperature, regridder_apply_kws):
     pyramid.to_zarr(MemoryStore())
 
 
+def test_regridded_pyramid_with_weights(temperature):
+    pytest.importorskip('xesmf')
+    levels = 2
+    weights_pyramid = generate_weights_pyramid(temperature.isel(time=0), levels)
+    pyramid = pyramid_regrid(
+        temperature, levels=levels, weights_pyramid=weights_pyramid, other_chunks={'time': 2}
+    )
+    assert pyramid.ds.attrs['multiscales']
+    assert len(pyramid.ds.attrs['multiscales'][0]['datasets']) == levels
+    pyramid.to_zarr(MemoryStore())
+
+
 def test_make_grid_ds():
 
     grid = make_grid_ds(0, pixels_per_tile=8)
     lon_vals = grid.lon_b.values
     assert np.all((lon_vals[-1, :] - lon_vals[0, :]) < 0.001)
+
+
+@pytest.mark.parametrize('levels', [1, 2])
+@pytest.mark.parametrize('method', ['bilinear', 'conservative'])
+def test_generate_weights_pyramid(temperature, levels, method):
+    weights_pyramid = generate_weights_pyramid(temperature.isel(time=0), levels, method=method)
+    assert weights_pyramid.ds.attrs['levels'] == levels
+    assert weights_pyramid.ds.attrs['regrid_method'] == method
+    assert set(weights_pyramid['0'].ds.data_vars) == {'S', 'col', 'row'}
+    assert 'n_in' in weights_pyramid['0'].ds.attrs and 'n_out' in weights_pyramid['0'].ds.attrs
