97c90ba17e50a39e23bbd54a03fa234e9fe139f4
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-01-12 13:48:39 -0500

    Setup continuous benchmarking workflow with pytest-codspeed (#85)

--- tests/test_pyramids.py
+++ tests/test_pyramids.py
@@ -14,19 +14,19 @@ def temperature():
     return ds
 
 
-def test_xarray_coarsened_pyramid(temperature):
+def test_xarray_coarsened_pyramid(temperature, benchmark):
     factors = [4, 2, 1]
-    pyramid = pyramid_coarsen(temperature, dims=('lat', 'lon'), factors=factors, boundary='trim')
+    pyramid = benchmark(lambda:  pyramid_coarsen(temperature, dims=('lat', 'lon'), factors=factors, boundary='trim'))
     assert pyramid.ds.attrs['multiscales']
     assert len(pyramid.ds.attrs['multiscales'][0]['datasets']) == len(factors)
     pyramid.to_zarr(MemoryStore())
 
 
-def test_reprojected_pyramid(temperature):
+def test_reprojected_pyramid(temperature, benchmark):
     pytest.importorskip('rioxarray')
     levels = 2
     temperature = temperature.rio.write_crs('EPSG:4326')
-    pyramid = pyramid_reproject(temperature, levels=2)
+    pyramid = benchmark(lambda:  pyramid_reproject(temperature, levels=2))
     assert pyramid.ds.attrs['multiscales']
     assert len(pyramid.ds.attrs['multiscales'][0]['datasets']) == levels
     assert pyramid.ds.attrs['multiscales'][0]['datasets'][0]['crs'] == 'EPSG:3857'
@@ -34,11 +34,11 @@ def test_reprojected_pyramid(temperature):
 
 
 @pytest.mark.parametrize('regridder_apply_kws', [None, {'keep_attrs': False}])
-def test_regridded_pyramid(temperature, regridder_apply_kws):
+def test_regridded_pyramid(temperature, regridder_apply_kws, benchmark):
     pytest.importorskip('xesmf')
-    pyramid = pyramid_regrid(
+    pyramid = benchmark(lambda:  pyramid_regrid(
         temperature, levels=2, regridder_apply_kws=regridder_apply_kws, other_chunks={'time': 2}
-    )
+    ))
     assert pyramid.ds.attrs['multiscales']
     expected_attrs = (
         temperature['air'].attrs
@@ -50,30 +50,30 @@ def test_regridded_pyramid(temperature, regridder_apply_kws):
     pyramid.to_zarr(MemoryStore())
 
 
-def test_regridded_pyramid_with_weights(temperature):
+def test_regridded_pyramid_with_weights(temperature, benchmark):
     pytest.importorskip('xesmf')
     levels = 2
     weights_pyramid = generate_weights_pyramid(temperature.isel(time=0), levels)
-    pyramid = pyramid_regrid(
+    pyramid = benchmark(lambda:  pyramid_regrid(
         temperature, levels=levels, weights_pyramid=weights_pyramid, other_chunks={'time': 2}
-    )
+    ))
     assert pyramid.ds.attrs['multiscales']
     assert len(pyramid.ds.attrs['multiscales'][0]['datasets']) == levels
     pyramid.to_zarr(MemoryStore())
 
 
 @pytest.mark.parametrize('projection', ['web-mercator', 'equidistant-cylindrical'])
-def test_make_grid_ds(projection):
+def test_make_grid_ds(projection, benchmark):
 
-    grid = make_grid_ds(0, pixels_per_tile=8, projection=projection)
+    grid = benchmark(lambda:  make_grid_ds(0, pixels_per_tile=8, projection=projection))
     lon_vals = grid.lon_b.values
     assert np.all((lon_vals[-1, :] - lon_vals[0, :]) < 0.001)
     assert grid.attrs['title'] == 'Web Mercator Grid' if projection == 'web-mercator' else 'Equidistant Cylindrical Grid'
 
 @pytest.mark.parametrize('levels', [1, 2])
 @pytest.mark.parametrize('method', ['bilinear', 'conservative'])
-def test_generate_weights_pyramid(temperature, levels, method):
-    weights_pyramid = generate_weights_pyramid(temperature.isel(time=0), levels, method=method)
+def test_generate_weights_pyramid(temperature, levels, method, benchmark):
+    weights_pyramid = benchmark(lambda:  generate_weights_pyramid(temperature.isel(time=0), levels, method=method))
     assert weights_pyramid.ds.attrs['levels'] == levels
     assert weights_pyramid.ds.attrs['regrid_method'] == method
     assert set(weights_pyramid['0'].ds.data_vars) == {'S', 'col', 'row'}
